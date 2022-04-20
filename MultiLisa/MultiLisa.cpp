af09477fdd751453698b0dea140fd0eeb9486a45
Даниил Левыкин <dan2807.1@gmail.com>
2022-04-20 08:10:39 +0300

    update

--- MultiLisa/MultiLisa.cpp
+++ MultiLisa/MultiLisa.cpp
@@ -1,13 +1,7 @@
 ﻿#include <iostream>
 #include <windows.h>
+#include <algorithm>
 using namespace std;
-
-struct paramsStruct {
-	int arraySize;
-	int maxNumA;
-	int* arrayInt;
-	HANDLE hSemaphore;
-};
 
 DWORD WINAPI work(LPVOID array);
 DWORD WINAPI MultiElement(LPVOID array);
@@ -28,76 +22,63 @@ int main()
 
 	cout << "Введите размерность массива: ";
 	cin >> arraySize;
-	int* arrayInt = new int[arraySize];
+	arraySize += 5;
+	int* array = new int[arraySize];
+	array[0] = arraySize;
 
-	cout << "Введите " << arraySize << " чесел:" << endl;
-	for (int i = 0; i < arraySize; i++)
-		cin >> arrayInt[i];
+	cout << "Введите " << arraySize-5 << " чесел:" << endl;
+	for (int i = 5; i < arraySize; i++)
+		cin >> array[i];
 
-	cout << "Размерность массива: " << arraySize << endl;
+	cout << "Размерность массива: " << arraySize-5 << endl;
 	cout << "Массив: ";
-	for (int i = 0; i < arraySize; i++)
-		cout << arrayInt[i] << " ";
+	for (int i = 5; i < arraySize; i++)
+		cout << array[i] << " ";
 	cout << endl;
 
 	cout << "Введите число k <= размеру массива: ";
-	cin >> k;
-	if (k > arraySize)
-		k = arraySize;
+	cin >> array[1];
+	array[1] += 5;
+	if (array[1] > arraySize)
+		array[1] = arraySize;
 
 	cout << "Введите максимальное число A: ";
-	cin >> maxNumA;
+	cin >> array[2];
 
-	hSemaphore = CreateSemaphore(NULL, 0, arraySize, NULL);
+	hSemaphore = CreateSemaphore(NULL, 0, arraySize-5, NULL);
 	if (hSemaphore == NULL)
 		return GetLastError();
+	array[3] = (int)hSemaphore;
 
-	struct paramsStruct* params = (struct paramsStruct*)malloc(sizeof(struct paramsStruct));
-	params->arraySize = arraySize;
-	params->maxNumA = maxNumA;
-	params->arrayInt = new int[arraySize];
-	params->arrayInt = arrayInt;
-	params->hSemaphore = hSemaphore;
-
-	hWork = CreateThread(NULL, 0, work, (void*)params, 0, &IdThread);
+	hWork = CreateThread(NULL, 0, work, (void*)array, 0, &IdThread);
 	if (hWork == NULL)
 		return GetLastError();
 
 	hBinSemaphore = CreateSemaphore(NULL, 0, 1, NULL);
 	if (hBinSemaphore == NULL)
 		return GetLastError();
+	array[4] = (int)hBinSemaphore;
 
-	struct paramsStruct* params1 = (struct paramsStruct*)malloc(sizeof(struct paramsStruct));
-	params1->arraySize = arraySize;
-	params1->maxNumA = k;
-	params1->arrayInt = new int[arraySize];
-	params1->arrayInt = arrayInt;
-	params1->hSemaphore = hBinSemaphore;
-
-	hMultiElement = CreateThread(NULL, 0, MultiElement, (void*)params1, 0, &IdThread);
+	hMultiElement = CreateThread(NULL, 0, MultiElement, (void*)array, 0, &IdThread);
 	if (hMultiElement == NULL)
 		return GetLastError();
 
-	for (int i = 0; i < arraySize; i++) {
-		WaitForSingleObject(hSemaphore, INFINITE);
-		cout << params->arrayInt[i] << " \a" << flush;
-		if (i + 1 == k)
-			ReleaseSemaphore(hBinSemaphore, 1, NULL);
+	for (int i = 5; i < arraySize; i++) {
+		WaitForSingleObject((HANDLE)array[3], INFINITE);
+		cout << array[i] << " \a" << flush;
+		if (i + 1 == array[1])
+			ReleaseSemaphore((HANDLE)array[4], 1, NULL);
 	}
 	cout << endl;
 
-	arrayInt = params->arrayInt;
-
 	cout << "Массив после изменения: ";
-	for (int i = 0; i < arraySize; i++)
-		cout << arrayInt[i] << " ";
+	for (int i = 5; i < arraySize; i++)
+		cout << array[i] << " ";
 
 	WaitForSingleObject(hWork, INFINITE);
 	CloseHandle(hWork);
 	WaitForSingleObject(hMultiElement, INFINITE);
 	CloseHandle(hMultiElement);
-	free(params);
-	free(params1);
 
 	cout << endl;
 	system("pause");
@@ -107,35 +88,35 @@ DWORD WINAPI work(LPVOID array) {
 	int sleepTime;
 	cout << "Время сна в милисекундах: ";
 	cin >> sleepTime;
-	struct paramsStruct* params = (struct paramsStruct*)array;
-	int reverseArrayNum = params->arraySize;
-	int offset = 0;
-	int* tmp = new int[params->arraySize];
-	for (int i = 0; i < params->arraySize; i++) {
-		int num = params->arrayInt[i];
-		if (num < params->maxNumA) {
-			params->arrayInt[offset] = num;
+	int* arrayL = (int*)array;
+	int reverseArrayNum = arrayL[0];
+	int offset = 5;
+	int* tmp = new int[arrayL[0]-5];
+	for (int i = 0; i < arrayL[0] - 5; i++)
+		tmp[i] = arrayL[i + 5];
+	for (int i = 0; i < arrayL[0]-5; i++) {
+		int num = tmp[i];
+		if (num < arrayL[2]) {
+			arrayL[offset] = num;
 			offset++;
 		}
 		else {
 			reverseArrayNum--;
-			tmp[reverseArrayNum] = num;
+			arrayL[reverseArrayNum] = num;
 		}
-		ReleaseSemaphore(params->hSemaphore, 1, NULL);
+		ReleaseSemaphore((HANDLE)arrayL[3], 1, NULL);
 		Sleep(sleepTime);
 	}
-	for (offset; offset < params->arraySize; offset++)
-		params->arrayInt[offset] = tmp[offset];
 	return 0;
 }
 
 DWORD WINAPI MultiElement(LPVOID array) {
-	struct paramsStruct* params = (struct paramsStruct*)array;
-	int k = params->maxNumA;
-	WaitForSingleObject(params->hSemaphore, INFINITE);
+	int* arrayL = (int*)array;
+	int k = arrayL[1]-1;
+	WaitForSingleObject((HANDLE)arrayL[4], INFINITE);
 	int res = 1;
-	for (int i = 0; i <= k; i++) {
-		res *= params->arrayInt[i];
+	for (int i = 5; i <= k; i++) {
+		res *= arrayL[i];
 	}
 	cout << "Итоговое произведение: " << res << endl;
 	return 0;
