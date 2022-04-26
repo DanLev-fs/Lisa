fb0ad5e2190e658170923529b401030bc5099adb
Даниил Левыкин <dan2807.1@gmail.com>
2022-04-26 21:52:42 +0300

    Вадим заткнись

--- Lisa/Lisa.cpp
+++ Lisa/Lisa.cpp
@@ -4,87 +4,71 @@ using namespace std;
 
 int main()
 {
-	setlocale(LC_ALL, "russian");
+    setlocale(LC_ALL, "russian");
 
-	int arraySize;			// Размер массива
-	int reverseArrayNum;	//
-	int offset = 0;			//
-	float maxNumA;			// Число A
-	float k;				//
+    int arraySize;          // Размер массива
+    int reverseArrayNum;    //
+    int offset = 0;         //
+    float maxNumA;          // Число A
+    float k;                //
 
-	cout << "Введите размерность массива: ";		// Задаём размер масива
-	cin >> arraySize;								//
-	reverseArrayNum = arraySize;					//
-	float* arrayInt = new float[arraySize];			// Создаём массивы
-	float* tmp = new float[arraySize];				//
+    cout << "Введите размерность массива: ";    // Задаём размер масива
+    cin >> arraySize;                           //
+    reverseArrayNum = arraySize;                //
+    float* arrayInt = new float[arraySize];     // Создаём массивы
+    float* tmp = new float[arraySize];          //
 
-	cout << "Введите " << arraySize << " чесел:" << endl;	//
-	for (int i = 0; i < arraySize; i++)						// Вводим числа
-		cin >> arrayInt[i];									//
+    cout << "Введите " << arraySize << " чесел:" << endl;   //
+    for (int i = 0; i < arraySize; i++)                     // Вводим числа
+        cin >> arrayInt[i];                                 //
 
-	cout << "Размерность массива: " << arraySize << endl;	//
-	cout << "Массив: ";										//
-	for (int i = 0; i < arraySize; i++)						// Выводим данные о массиве
-		cout << arrayInt[i] << " ";							//
-	cout << endl;											//
+    cout << "Размерность массива: " << arraySize << endl;   //
+    cout << "Массив: ";                                     //
+    for (int i = 0; i < arraySize; i++)                     // Выводим данные о массиве
+        cout << arrayInt[i] << " ";                         //
+    cout << endl;                                           //
 
-	cout << "Введите число k <= " << arraySize << ": ";			//
-	cin >> k;													// Вводим число k
-	while (k > arraySize) {										//
-		cout << "Ошибка ввода" << endl;							//
-		cout << "Введите число k <= " << arraySize << ": ";		//
-		cin >> k;												// Вводим число k
-	}
+    cout << "Введите число k <= " << arraySize << ": ";         //
+    cin >> k;                                                   // Вводим число k
+    while (k > arraySize) {                                     //
+        cout << "Ошибка ввода" << endl;                         //
+        cout << "Введите число k <= " << arraySize << ": ";     //
+        cin >> k;                                               // Вводим число k
+    }
 
-	cout << "Введите максимальное число A: ";	//
-	cin >> maxNumA;								// Вводим число A
+    cout << "Введите максимальное число A: ";   //
+    cin >> maxNumA;                             // Вводим число A
 
-	int sleepTime;							//
-	cout << "Время сна в милисекундах: ";	// Вводим время сна между итерациями
-	cin >> sleepTime;						//
+    int sleepTime;                          //
+    cout << "Время сна в милисекундах: ";   // Вводим время сна между итерациями
+    cin >> sleepTime;                       //
 
-	for (int i = 0; i < arraySize; i++) {	//
-		float num = arrayInt[i];			//
-		if (num < maxNumA) {				// Ищем элементы меньше A
-			tmp[offset] = num;				// Записываем в лево
-			offset++;						//
-			cout << num << " \a" << flush;	// Выводим новый элемент
-			Sleep(sleepTime);				// Спим указанное время
-		}									//
-	}										//
 
-	for (int i = 0; i < arraySize; i++) {	//
-		float num = arrayInt[i];			//
-		if (num == maxNumA) {				// Проверяем элементы равные A
-			tmp[offset] = num;				// Записываем
-			offset++;						//
-			cout << num << " \a" << flush;	// Выводим новый элемент
-			Sleep(sleepTime);				// Спим указанное время
-		}									//
-	}										//
+    for (int i = 0; i < arraySize; i++) {   //
+        int num = arrayInt[i];              //
+        if (num < maxNumA) {                // Ищем элементы меньше A
+            tmp[offset] = num;              // Записываем в лево
+            offset++;                       //
+        }                                   //
+        else {                              //
+            reverseArrayNum--;              // Записываем в право
+            tmp[reverseArrayNum] = num;     //
+        }                                   //
+    }                                       //
 
-	for (int i = 0; i < arraySize; i++) {	//
-		float num = arrayInt[i];			//
-		if (num > maxNumA) {				// Ищем элементы больше A
-			tmp[offset] = num;				// Записываем в право
-			offset++;						//
-			cout << num << " \a" << flush;	// Выводим новый элемент
-			Sleep(sleepTime);				// Спим указанное время
-		}									//
-	}										//
-	arrayInt = tmp;							// Возвращение значения из временного массива
-	cout << endl;							//
+    arrayInt = tmp;     // Возвращение значения из временного массива
+    cout << endl;       //
 
-	float res = 1;					//
-	for (int i = 0; i < k; i++)		// Произведение
-		res *= arrayInt[i];			//
+    float res = 1;                  //
+    for (int i = 0; i < k; i++)     // Произведение
+        res *= arrayInt[i];         //
 
-	cout << "Массив после изменения: ";	//
-	for (int i = 0; i < arraySize; i++)	// Выводим итоговый массив
-		cout << arrayInt[i] << " ";		//
-	cout << endl;
+    cout << "Массив после изменения: ";     //
+    for (int i = 0; i < arraySize; i++)     // Выводим итоговый массив
+        cout << arrayInt[i] << " ";         //
+    cout << endl;                           //
 
-	cout << "Итоговое произведение: " << res << endl;	// Выводм итоговое произведение
+    cout << "Итоговое произведение: " << res << endl;   // Выводм итоговое произведение
 
-	system("pause");
+    system("pause");
 }
--- MultiLisa/MultiLisa.cpp
+++ MultiLisa/MultiLisa.cpp
@@ -1,155 +1,151 @@
 ﻿#include <iostream>
 #include <windows.h>
-#include <algorithm>
 using namespace std;
 
 DWORD WINAPI work(LPVOID array);
 DWORD WINAPI MultiElement(LPVOID array);
 
+struct threadParametrs {
+    float* array;
+    float maxNumA;
+    int arraySize;
+    int k;
+    HANDLE hWorkSemaphore;
+    HANDLE hMultiElementBinSemaphore;
+    HANDLE hMultiElementBinSemaphorePrint;
+};
+
 int main()
 {
-	setlocale(LC_ALL, "russian");
+    setlocale(LC_ALL, "russian");
 
-	int arraySize;		// Размер массива
-	float maxNumA;		// Число A 
-	float k;			// Число k
+    threadParametrs* params = (threadParametrs*)malloc(sizeof(threadParametrs));
 
-	HANDLE hWork;				// Дескриптор потока work
-	HANDLE hMultiElement;		// Дескриптор потока MultiElement
-	HANDLE hSemaphore;			// Дескриптор семафора для потока work
-	HANDLE hBinSemaphore;		// Дескриптор бинарного семафора для потока MultiElement
-	HANDLE hBinSemaphorePrint;	// Дескриптор бинарного семафора для потока MultiElement
+    HANDLE hWork;           // Дескриптор потока work
+    HANDLE hMultiElement;   // Дескриптор потока MultiElement
 
-	cout << "Введите размерность массива: ";		//
-	cin >> arraySize;								// Задаём размер массива
-	arraySize += 6;									// и
-	float* array = new float[arraySize];			// создаём массив
-	array[0] = arraySize;							// В первом элементе массива указываем размер массива
+    cout << "Введите размерность массива: ";        //
+    cin >> params->arraySize;                       // Задаём размер массива и
+    while (params->arraySize <= 0) {                 //
+        cout << "Ошибка ввода" << endl;             //
+        cout << "Введите размерность массива: ";    //
+        cin >> params->arraySize;                   // Задаём размер массива и
+    }                                               //
+    params->array = new float[params->arraySize];   // создаём массив
 
-	cout << "Введите " << arraySize - 6 << " чесел:" << endl;	//
-	for (int i = 6; i < arraySize; i++)							// Вводим числа
-		cin >> array[i];										//
+    cout << "Введите " << params->arraySize << " чесел:" << endl;   //
+    for (int i = 0; i < params->arraySize; i++)                     // Вводим числа
+        cin >> params->array[i];                                    //
 
-	cout << "Размерность массива: " << arraySize - 5 << endl;	//
-	cout << "Массив: ";											//
-	for (int i = 6; i < arraySize; i++)							// Выводим массив
-		cout << array[i] << " ";								//
-	cout << endl;												//
+    cout << "Размерность массива: " << params->arraySize << endl;   //
+    cout << "Массив: ";                                             //
+    for (int i = 0; i < params->arraySize; i++)                     // Выводим массив
+        cout << params->array[i] << " ";                            //
+    cout << endl;                                                   //
 
-	cout << "Введите число k <= " << arraySize - 6 << ": ";		//
-	cin >> array[1];											// Вводим число k
-	array[1] += 6;												//
-	while (array[1] > arraySize) {								//
-		cout << "Ошибка ввода" << endl;							//
-		cout << "Введите число k <= " << arraySize- 6 << ": ";	//
-		cin >> array[1];										// Вводим число k
-	}
+    cout << "Введите число k от 0 до " << params->arraySize << ": ";        // Вводим число k
+    cin >> params->k;                                                       //
+    while (params->k > params->arraySize && params->k >= 0) {               //
+        cout << "Ошибка ввода" << endl;                                     //
+        cout << "Введите число k от 0 до " << params->arraySize << ": ";    //
+        cin >> params->k;                                                   // Вводим число k
+    }                                                                       //
 
-	cout << "Введите максимальное число A: ";	//
-	cin >> array[2];							// Вводим число A
+    cout << "Введите максимальное число A: ";   //
+    cin >> params->maxNumA;                     // Вводим число A
 
-	hSemaphore = CreateSemaphore(NULL, 0, arraySize- 6, NULL);	//
-	if (hSemaphore == NULL)										// Создаём семафор для потока work
-		return GetLastError();									//
-	array[3] = (int)hSemaphore;									// Сохраняем его в массив
+    params->hWorkSemaphore = CreateSemaphore(NULL, 0, params->arraySize, NULL); //
+    if (params->hWorkSemaphore == NULL)                                         // Создаём семафор для потока work
+        return GetLastError();                                                  //
+    hWork = CreateThread(NULL, 0, work, (void*)params, 0, NULL);                //
+    if (hWork == NULL)                                                          // Создаём поток work
+        return GetLastError();                                                  //
 
-	hWork = CreateThread(NULL, 0, work, (void*)array, 0, NULL);	//
-	if (hWork == NULL)											// Создаём поток work
-		return GetLastError();									//
+    params->hMultiElementBinSemaphore = CreateSemaphore(NULL, 0, 1, NULL);          //
+    if (params->hMultiElementBinSemaphore == NULL)                                  // Создаём бинарный семафор для потока MultiElement
+        return GetLastError();                                                      //
+    params->hMultiElementBinSemaphorePrint = CreateSemaphore(NULL, 0, 1, NULL);     //
+    if (params->hMultiElementBinSemaphorePrint == NULL)                             // Создаём бинарный семафор для потока MultiElement
+        return GetLastError();                                                      //
+    hMultiElement = CreateThread(NULL, 0, MultiElement, (void*)params, 0, NULL);    //
+    if (hMultiElement == NULL)                                                      // Создаём поток MultiElement
+        return GetLastError();                                                      //
 
-	hBinSemaphore = CreateSemaphore(NULL, 0, 1, NULL);		//
-	if (hBinSemaphore == NULL)								// Создаём бинарный семафор для потока MultiElement
-		return GetLastError();								//
-	array[4] = (int)hBinSemaphore;							// Сохраняем его в массив
-	hBinSemaphorePrint = CreateSemaphore(NULL, 0, 1, NULL);	//
-	if (hBinSemaphorePrint == NULL)							// Создаём бинарный семафор для потока MultiElement
-		return GetLastError();								//
-	array[5] = (int)hBinSemaphorePrint;						// Сохраняем его в массив
+    for (int i = 0; i < params->arraySize; i++) {                           //
+        WaitForSingleObject(params->hWorkSemaphore, INFINITE);              // Ждём сигнал нового элемента в массиве
+        cout << params->array[i] << " \a" << flush;                         // и выводм элемент
+        if (i + 1 == params->k)                                             // Проверяем количество прошедших элементов
+            ReleaseSemaphore(params->hMultiElementBinSemaphore, 1, NULL);   // Запускаем поток MultiElement
+    }                                                                       //
+    cout << endl;                                                           //
 
-	hMultiElement = CreateThread(NULL, 0, MultiElement, (void*)array, 0, NULL);	//
-	if (hMultiElement == NULL)													// Создаём поток MultiElement
-		return GetLastError();													//
+    cout << "Массив после изменения: ";         //
+    for (int i = 0; i < params->arraySize; i++) // Выводм иготовый массив
+        cout << params->array[i] << " ";        //
+    cout << endl;                               //
 
-	for (int i = 6; i < arraySize; i++) {					//
-		WaitForSingleObject((HANDLE)(int)array[3], INFINITE);	// Ждём сигнал нового элемента в массиве
-		cout << array[i] << " \a" << flush;					// и выводм элемент
-		if (i + 1 == array[1])								// Проверяем количество прошедших элементов
-			ReleaseSemaphore((HANDLE)(int)array[4], 1, NULL);	// Запускаем поток MultiElement
-	}														//
-	cout << endl;											//
+    ReleaseSemaphore(params->hMultiElementBinSemaphorePrint, 1, NULL);  // Запускаем поток MultiElement
 
-	cout << "Массив после изменения: ";	//
-	for (int i = 6; i < arraySize; i++)	// Выводм иготовый массив
-		cout << array[i] << " ";		//
-	cout << endl;
+    WaitForSingleObject(hWork, INFINITE);                   // Ждём завершения потоков
+    WaitForSingleObject(hMultiElement, INFINITE);           //
+    CloseHandle(hWork);                                     //
+    CloseHandle(hMultiElement);                             //
+    CloseHandle(params->hWorkSemaphore);                    //
+    CloseHandle(params->hMultiElementBinSemaphore);         // Удаляем дескрипторы
+    CloseHandle(params->hMultiElementBinSemaphorePrint);    //
 
-	ReleaseSemaphore((HANDLE)(int)array[5], 1, NULL);	// Запускаем поток MultiElement
-
-	WaitForSingleObject(hWork, INFINITE);			// Ждём завершения потоков
-	WaitForSingleObject(hMultiElement, INFINITE);	//
-	CloseHandle(hWork);								//
-	CloseHandle(hMultiElement);						// Удаляем дескрипторы
-
-	system("pause");
+    system("pause");
 }
 
 DWORD WINAPI work(LPVOID array) {
-	int sleepTime;							//
-	cout << "Время сна в милисекундах: ";	// Вводим время сна между итерациями
-	cin >> sleepTime;						//
+    int sleepTime;                              //
+    cout << "Время сна в милисекундах: ";       // Вводим время сна между итерациями
+    cin >> sleepTime;                           //
+    while (sleepTime <= 0) {                    //
+        cout << "Ошибка ввода" << endl;         //
+        cout << "Время сна в милисекундах: ";   //
+        cin >> sleepTime;                       //
+    }                                           //
 
-	float* arrayL = (float*)array;			// Приводим LPVOID к int*
-	int reverseArrayNum = arrayL[0];		// 
-	int offset = 6;							//
-	float* tmp = new float[arrayL[0] - 6];	// Создаём временный массив
+    threadParametrs* params = (threadParametrs*)array;
+    float* tmp = new float[params->arraySize];
+    int offset = 0;
+    int reverseArrayNum = params->arraySize;
 
-	for (int i = 0; i < arrayL[0] - 6; i++)	// Копируем данные во временный массив 
-		tmp[i] = arrayL[i + 6];				//
+    for (int i = 0; i < params->arraySize; i++)	// Копируем данные во временный массив 
+        tmp[i] = params->array[i];              //
 
-	for (int i = 0; i < arrayL[0]- 6; i++) {					//
-		float num = tmp[i];										//
-		if (num < arrayL[2]) {									// Ищем элементы меньше A
-			arrayL[offset] = num;								// Записываем в лево
-			offset++;											//
-			ReleaseSemaphore((HANDLE)(int)arrayL[3], 1, NULL);		// Сообщаем о завершении итерации
-			Sleep(sleepTime);									// Спим указанное время
-		}														//
-	}															//
+    for (int i = 0; i < params->arraySize; i++) {   //
+        int num = tmp[i];                           //
+        if (num < params->maxNumA) {                // Ищем элементы меньше A
+            params->array[offset] = num;            // Записываем в лево
+            offset++;                               //
+        }                                           //
+        else {                                      //
+            reverseArrayNum--;                      // Записываем в право
+            params->array[reverseArrayNum] = num;   //
+        }                                           //
+    }
 
-	for (int i = 0; i < arrayL[0] - 6; i++) {					//
-		float num = tmp[i];										//
-		if (num == arrayL[2]) {									// Проверяем элементы равные A
-			arrayL[offset] = num;								// Записываем
-			offset++;											//
-			ReleaseSemaphore((HANDLE)(int)arrayL[3], 1, NULL);	// Сообщаем о завершении итерации
-			Sleep(sleepTime);									// Спим указанное время
-		}														//
-	}															//
+    for (int i = 0; i < params->arraySize; i++) {           //
+        ReleaseSemaphore(params->hWorkSemaphore, 1, NULL);	// Сообщаем о завершении итерации
+        Sleep(sleepTime);								    // Спим указанное время
+    }                                                       //
 
-	for (int i = 0; i < arrayL[0] - 6; i++) {					//
-		float num = tmp[i];										//
-		if (num > arrayL[2]) {									// Ищем элементы больше A
-			arrayL[offset] = num;								// Записываем в право
-			offset++;											//
-			ReleaseSemaphore((HANDLE)(int)arrayL[3], 1, NULL);	// Сообщаем о завершении итерации
-			Sleep(sleepTime);									// Спим указанное время
-		}														//
-	}															//
-
-	return 0;
+    return 0;
 }
 
 DWORD WINAPI MultiElement(LPVOID array) {
-	float* arrayL = (float*)array;	// Приводим LPVOID к int*
-	int k = arrayL[1];			//
+    threadParametrs* params = (threadParametrs*)array;  //
 
-	WaitForSingleObject((HANDLE)(int)arrayL[4], INFINITE);	// Ждём сигнал
+    WaitForSingleObject(params->hMultiElementBinSemaphore, INFINITE);   // Ждём сигнал
 
-	float res = 1;					//
-	for (int i = 6; i < k; i++)	// Произведение
-		res *= arrayL[i];			//
+    float res = 1;                      //
+    for (int i = 6; i < params->k; i++) // Произведение
+        res *= params->array[i];        //
 
-	WaitForSingleObject((HANDLE)(int)arrayL[5], INFINITE);	// Ждём сигнал
-	cout << "Итоговое произведение: " << res << endl;	// Выводм итоговое произведение
-	return 0;
+    WaitForSingleObject(params->hMultiElementBinSemaphorePrint, INFINITE);  // Ждём сигнал
+    cout << "Итоговое произведение: " << res << endl;                       // Выводм итоговое произведение
+
+    return 0;
 }
